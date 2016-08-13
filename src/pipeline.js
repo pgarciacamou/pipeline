e486920dc5462499a2e1d5137d2c7ce4802c7f46
Pablo Garcia <pgarciacamou@gmail.com>
2016-08-13 14:04:15 -0700

    added pipeline.js code

---
+++ src/pipeline.js
@@ -0,0 +1,62 @@
+// HELPERS
+function uniqueID() {
+  return Object.freeze({});
+}
+
+// PIPE
+function pipe(callback=null, _pipes={cbs:[]}){
+  callback && _pipes.cbs.push(callback);
+  return {
+    pipe: cb => pipe(cb, _pipes),
+    run: res => {
+      var cb, i=0;
+      while(cb = _pipes.cbs[i++]){
+        temp = cb(res);
+        if(temp && temp.pipe) temp.pipe(_ => temp = _).run();
+        if(temp === pipe.skip) continue;
+        res = temp;
+        if(res === pipe.stop) break;
+      }
+      return res;
+    }
+  };
+}
+
+// COMMANDS
+pipe.stop = uniqueID();
+pipe.skip = uniqueID();
+
+// EXTRA FUNCTIONALITY
+pipe.pipeline = function (callback){
+  var _pipe = pipe();
+  callback(_pipe.run);
+  return _pipe;
+};
+pipe.preciseBuffer = function(exactly=0){
+  var buffer = [];
+  return _ => {
+    buffer.push(_);
+    if(buffer.length < exactly) return pipe.stop;
+    return buffer.splice(0);
+  };
+};
+pipe.continuousBuffer = function(upperBoundLimit=100){
+  var buffer = [];
+  return _ => {
+    buffer.push(_);
+    if(buffer.length > upperBoundLimit) buffer.splice(0,buffer.length - upperBoundLimit);
+    return buffer;
+  };
+};
+pipe.latest = function(n) {
+  return _ => {
+    return _.slice(-n);
+  };
+};
+pipe.log = function(msg, process = _ => _) {
+  return _ => {
+    let log = [process(_)]
+    msg && log.unshift(msg);
+    return console.log.apply(console, log);
+  };
+};
