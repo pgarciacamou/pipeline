98689e092dce74122ffe5d33ca557b9fdaf21af3
Pablo Garcia <pgarciacamou@gmail.com>
2016-08-25 22:47:52 -0700

    separated helpers into different folder

--- build.js
+++ build.js
@@ -1,55 +1,6 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
-
-// HELPERS
-// ---------------------------------------
-
-function run(command, fn) {
-  return new Promise(function(resolve, reject){
-    exec(command, function(err, stdout, stderr){
-      if(fn) {
-        fn(resolve, reject);
-      } else {
-        if(!err) resolve();
-        else {
-          console.log(stderr);
-          reject();
-        }
-      }
-    });
-  });
-}
-
-// makes execution synchronous
-function execSync(){
-  var cbs = []
-  var ret;
-  var timeoutId;
-
-  function go(index) {
-    index = index || 0;
-    cbs[index] && run(cbs[index].cmd, cbs[index].fn).then(function(){
-      go(++index);
-    });
-  }
-
-  return ret = {
-    next: function(cmd, fn){
-      clearTimeout(timeoutId);
-
-      cbs.push({
-        cmd: cmd,
-        fn: fn
-      });
-
-      timeoutId = setTimeout(go);
-      return ret;
-    }
-  };
-}
-
-// START BUILD PROCESS
-// ---------------------------------------
+var execSync = require('./build/sequentialExecution.js');
 
 execSync()
 
